cae86231dfa638c0a1e142842aaa883c29924db4
LIFE <dkrshw65@gmail.com>
2020-04-02 14:16:07 -0400

    deploy

--- server.js
+++ server.js
@@ -1,39 +1,87 @@
+// const express = require('express');
+// const mongoose= require('mongoose');
+// const cors    = require('cors');
+// const path    = require('path');
+// require('dotenv').config();
+
+// // App
+// const app = express();
+
+// app.use(express.static('./client/build'))
+
+// // Middle for URL parser
+// app.use(express.json());
+// app.use(cors());
+
+
+// const uri = process.env.MONGO_URI;
+
+// mongoose.connect(uri, {
+//     useNewUrlParser: true, useUnifiedTopology: true
+// });
+
+// const connection = mongoose.connection;
+// connection.once('open', () => console.log('MongoDB established!'))
+// connection.on('error', err => console.log(`Error: ${err}`))
+
+// // Routers
+// app.use('/api/newtask', require('./routes/api/newtask'));
+
+// // accessing direct url like exampel.com/user
+// app.use('/', (req, res) => { 
+//     return res.sendFile(path.join(__dirname, './client/build/index.html')); 
+// });
+
+// // PORT
+// const PORT = process.env.PORT | 5001;
+
+// // Listening
+// app.listen(PORT, () => `Server is running on ${PORT}`)
+
+
+
+
 const express = require('express');
-const mongoose= require('mongoose');
 const cors    = require('cors');
+const mongoose= require('mongoose');
 const path    = require('path');
+
 require('dotenv').config();
 
 // App
 const app = express();
 
-app.use(express.static('./client/build'))
-
-// Middle for URL parser
-app.use(express.json());
+// Middleware
 app.use(cors());
 
+// Body Parser, MW
+app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
 
-const uri = process.env.MONGO_URI;
-
-mongoose.connect(uri, {
-    useNewUrlParser: true, useUnifiedTopology: true
-});
+// mongoose
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri || 'mongodb://localhost:27017/exe_tracker', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
-connection.once('open', () => console.log('MongoDB established!'))
+connection.once('open', () => console.log('MongoDB esatablished!'));
 connection.on('error', err => console.log(`Error: ${err}`))
+
 
 // Routers
 app.use('/api/newtask', require('./routes/api/newtask'));
 
-// accessing direct url like exampel.com/user
-app.use('/', (req, res) => { 
-    return res.sendFile(path.join(__dirname, './client/build/index.html')); 
-});
+// Heroku
 
-// PORT
-const PORT = process.env.PORT | 5001;
+if( process.env.NODE_ENV === 'production')
+{
+    app.use(express.static('./client/build'))
 
-// Listening
-app.listen(PORT, () => `Server is running on ${PORT}`)
+    // accessing direct url like exampel.com/user
+    app.use('/*', (req, res) => { 
+        return res.sendFile(path.join(__dirname, './client/build/index.html')); 
+    });
+}
+
+// PORTS
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
