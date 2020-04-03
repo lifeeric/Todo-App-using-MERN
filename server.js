883242e62aca6360dc4328a721d6bd9248b2e4c9
LIFE <dkrshw65@gmail.com>
2020-04-02 23:27:18 -0400

    url fixingxx

--- server.js
+++ server.js
@@ -1,76 +1,29 @@
-// const express = require('express');
-// const mongoose= require('mongoose');
-// const cors    = require('cors');
-// const path    = require('path');
-// require('dotenv').config();
-
-// // App
-// const app = express();
-
-
-// // Middle for URL parser
-// app.use(cors());
-// app.use(express.json());
-
-
-// const uri = process.env.ATLAS_URI;
-
-// mongoose.connect(uri, {
-//     useNewUrlParser: true, useUnifiedTopology: true
-// });
-
-// const connection = mongoose.connection;
-// connection.once('open', () => console.log('MongoDB established!'))
-// connection.on('error', err => console.log(`Error: ${err}`))
-
-// // Routers
-// app.use('/api/newtask', require('./routes/api/newtask'));
-
-// if ( process.env.NODE_ENV === 'production' )
-// {
-//     app.use(express.static('./client/build'));
-
-//     //access direct url like /abc
-//     app.use('/*', (req, res) => {
-//         return res.sendFile(path.join(__dirname, './client/build/index.html'));
-//     })
-// }
-// // PORT
-// const PORT = process.env.PORT | 5001;
-
-// // Listening
-// app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
-
-
-
 const express = require('express');
-const cors    = require('cors');
 const mongoose= require('mongoose');
+const cors    = require('cors');
 const path    = require('path');
-
 require('dotenv').config();
 
 // App
 const app = express();
 
-// Middleware
+
+// Middle for URL parser
 app.use(cors());
-
-// Body Parser, MW
 app.use(express.json());
-// app.use(express.urlencoded({extended: false}));
 
-// mongoose
+
 const uri = process.env.ATLAS_URI;
+
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
-connection.once('open', () => console.log('MongoDB esatablished!'));
+connection.once('open', () => console.log('MongoDB established!'))
 connection.on('error', err => console.log(`Error: ${err}`))
-
 
 // Routers
 app.use('/api/newtask', require('./routes/api/newtask'));
+
 
 // Heroku
 
@@ -84,6 +37,11 @@ if( process.env.NODE_ENV === 'production')
     });
 }
 
-// PORTS
-const PORT = process.env.PORT || 5001;
+// PORT
+const PORT = process.env.PORT | 5001;
+
+// Listening
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+
+
