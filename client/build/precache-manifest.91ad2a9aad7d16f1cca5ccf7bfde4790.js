71cb3e992be6654c54826de0fc21512171742475
LIFE <dkrshw65@gmail.com>
2020-04-02 23:52:34 -0400

    server

---
+++ client/build/precache-manifest.91ad2a9aad7d16f1cca5ccf7bfde4790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f16ed817b5a16e4d024cd3099ec364",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45f267f6138b7218983a",
+    "url": "/static/css/main.70f4fa84.chunk.css"
+  },
+  {
+    "revision": "315a44381282d412a052",
+    "url": "/static/js/2.101ff7d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.101ff7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f267f6138b7218983a",
+    "url": "/static/js/main.2606c511.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
