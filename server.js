b99fc29c79ca076420a54d42412c7c9aecee634e
LIFE <dkrshw65@gmail.com>
2020-04-02 23:57:32 -0400

    production mode bug fixed

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
 const express = require('express');
-const cors    = require('cors');
 const mongoose= require('mongoose');
+const cors    = require('cors');
 const path    = require('path');
-
 require('dotenv').config();
 
 // App
 const app = express();
 
-// Middleware
+
+// Middle for URL parser
 app.use(cors());
-
-// Body Parser, MW
 app.use(express.json());
-// app.use(express.urlencoded({extended: false}));
 
 
-// mongoose
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect(uri, {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
 
 const connection = mongoose.connection;
-connection.once('open', () => console.log('MongoDB esatablished!'));
+connection.once('open', () => console.log('MongoDB established!'))
 connection.on('error', err => console.log(`Error: ${err}`))
-
 
 // Routers
 app.use('/api/newtask', require('./routes/api/newtask'));
 
-
-if( process.env.NODE_ENV === 'production')
+if ( process.env.NODE_ENV === 'production' )
 {
-    app.use(express.static('./client/build'))
+    app.use(express.static('./client/build'));
 
-    // accessing direct url like exampel.com/user
-    app.use('/*', (req, res) => { 
-        return res.sendFile(path.join(__dirname, './client/build/index.html')); 
-    });
+    //access direct url like /abc
+    app.use('/*', (req, res) => {
+        return res.sendFile(path.join(__dirname, './client/build/index.html'));
+    })
 }
-
 // PORT
-const PORT = process.env.PORT | 5001;
+const PORT = process.env.PORT || 5001;
 
 // Listening
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+
+
